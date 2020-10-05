ae4305ee7af4745012adf584ac8468cb97f4904b
Byung Jin Choi <lunaticholic@gmail.com>
2020-10-06 01:40:35 +0900

    반응형 헤더 웹사이트 만들기

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const toggleBtn = document.querySelector('.navbar_toogleBtn');
 const menu = document.querySelector('.navbar_menu');
 const icons = document.querySelector('.navbar_icons');
 
+//toggleBtn이 click이라는 이벤트가 일어날 경우 실행되는 방법
 toggleBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
   icons.classList.toggle('active');
